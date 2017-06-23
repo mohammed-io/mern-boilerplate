85ae142ae53be8682b9d4c76719b7fbfdde5eac6
Clayton Ray <iamclaytonray@gmail.com>
2017-06-23 11:19:55 -0400

    Add server.ts back in

---
+++ server/server.ts
@@ -0,0 +1,70 @@
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+import * as compression from 'compression';
+import * as logger from 'morgan';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
+import * as path from 'path';
+
+
+// API Routers
+import UserRouter from './router/UserRouter';
+import PostRouter from './router/PostRouter';
+
+
+// Server Class
+class Server {
+
+  public app: express.Application;
+
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
+
+  
+  // App Config
+  public config() {
+
+    const MONGO_URI: string = 'mongodb://localhost/mern-boilerplate'; 
+    mongoose.connect(MONGO_URI || process.env.MONGODB_URI);
+
+    // Middleware
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+    this.app.use(cookieParser());
+    this.app.use(logger('dev'));
+    this.app.use(compression());
+    this.app.use(helmet());
+    this.app.use(cors());
+
+    // CORS
+    this.app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+      res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      next();
+    });
+    
+  }
+
+  // App Routes
+  public routes(): void {
+
+    let router: express.Router;
+    router = express.Router();
+
+    this.app.use('/', router);
+    this.app.use('/api/v1/users', UserRouter);
+    this.app.use('/api/v1/posts', PostRouter);
+  }
+}
+
+
+// Export
+export default new Server().app;
